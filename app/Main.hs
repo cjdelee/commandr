a73f0fc1d64cfc909d27f5e9e135b6e2dd1191a1
cjdelee <cjdelee@gmail.com>
2022-05-24 15:35:56 -0700

    Built base code, displays a gtk window

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
 
 main :: IO ()
-main = someFunc
+main = do
+  Gtk.init Nothing
+
+  win <- Gtk.windowNew Gtk.WindowTypeToplevel               -- Set the variable win to be a new GTK window
+  Gtk.setContainerBorderWidth win 10                        -- Set the border width of win
+  Gtk.setWindowTitle win "Commandr"                         -- Set the window title of win
+  Gtk.setWindowResizable win False                          -- Determines if win can be resized
+  Gtk.setWindowDefaultWidth win 750                         -- Default window width
+  Gtk.setWindowDefaultHeight win 225                        -- Default window height
+  Gtk.setWindowWindowPosition win Gtk.WindowPositionCenter  --Sets the window's position on the screen
+  Gtk.windowSetDecorated win False                          -- Disable window decorations
+
+  Gtk.onWidgetDestroy win Gtk.mainQuit
+  #showAll win
+  Gtk.main
