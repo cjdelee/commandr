106bf6a4a31ce6ff37ad6aac1c786c3eca8be3f0
cjdelee <cjdelee@gmail.com>
2022-05-24 16:32:26 -0700

    Added images for buttons

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,22 @@ module Main where
 
 import qualified GI.Gtk as Gtk
 import Data.GI.Base
+import System.Directory
 
 main :: IO ()
 main = do
   Gtk.init Nothing
 
+-- Define variables
   win <- Gtk.windowNew Gtk.WindowTypeToplevel               -- Set the variable win to be a new GTK window
+  home <- getHomeDirectory
+  imgLogout <- Gtk.imageNewFromFile $ home ++ "/commandr/image/logout.png"
+  imgShutdown <- Gtk.imageNewFromFile $ home ++ "/commandr/image/shutdown.png"
+  imgCancel <- Gtk.imageNewFromFile $ home ++ "/commandr/image/cancel.png"
+  imgSuspend <- Gtk.imageNewFromFile $ home ++ "/commandr/image/suspend.png"
+  imgHibernate <- Gtk.imageNewFromFile $ home ++ "/commandr/image/hibernate.png"
+  imgRestart <- Gtk.imageNewFromFile $ home ++ "/commandr/image/restart.png"
+
   Gtk.setContainerBorderWidth win 10                        -- Set the border width of win
   Gtk.setWindowTitle win "Commandr"                         -- Set the window title of win
   Gtk.setWindowResizable win False                          -- Determines if win can be resized
